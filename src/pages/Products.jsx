ba7174758dc626ecf9d8a2c9f09c8111837fc2e8
Tobias Dedorsson <50805430+tobionesies@users.noreply.github.com>
2023-04-12 08:12:02 +0200

    Fetch Products fix

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,43 +3,41 @@ import React, { useState, useEffect } from 'react'
 const Products = () => {
 
   const [products, setProducts] = useState()
-  const fetchProducts = async () => {
 
+  const fetchProducts = async () => {
     try {
       const response = await fetch ('https://product-api-production-5f7f.up.railway.app/products/')
       const products = await response.json();
       setProducts(products)
+      return products
 
-      if(!response.ok) {
-        throw new Error('Could not fetch the data')} 
+      // if(!response.ok) {
+      //   throw new Error('Could not fetch the data')} 
     } catch (error) {
       setDefaultResultOrder(error.message)
     }
   }
 
   useEffect(() => {
-    fetchProducts()
+    fetchProducts();
   },[])
 
-  const productList = (products) => {
-    
-    products!=null 
-      ? products.map((product) => 
-        <div key={product['_id']}>
-          <h3>{product.title}</h3>
-          <p>{product.description}</p>
-          <h4>{product.price}</h4>
-          <i>{product.stock}</i>
-          <i>{product.category}</i>
-          <img src={product.image} alt="" />
-        </div>)
-      : <div>Error :c</div>
-  }
+ 
 
   return (
     <>
-      {productList(products)}
-      <h1>Products goes here..</h1>
+      { products!=null 
+          ? products.map((product) => 
+            <div key={product['_id']}>
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <h4>{product.price}</h4>
+              <i>{product.stock}</i>
+              <i>{product.category}</i>
+              <img src={product.image} alt="" />
+            </div>)
+          : <div>Error :c</div>
+      }
     </>
   )
 }
