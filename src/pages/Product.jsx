8d5036df1a2355bff98f7ef583d6a235d4b2d729
Léon Aysa <114478401+Arkanidas@users.noreply.github.com>
2023-04-12 22:22:39 +0200

    Product.jsx tillagd första gången

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React , { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
 
-const Product = () => {
+
+const Productfunction = () => {
+
+    const [item, setitem] = useState([])
+    const params = useParams();
+    
+    const Fetchproduct = async () => {
+      try {
+        const response = await fetch ('https://product-api-production-5f7f.up.railway.app/products/' + params.id)
+        const data = await response.json()
+        setitem(data)
+      
+    
+      } catch (error) {
+        return "an error has occured"
+      }
+    }
+  
+  let valör = "kr"
+
+useEffect(() => {
+  Fetchproduct()
+  
+},[])
+
+
+
   return (
-    <div>Product</div>
+<div className='item-container'>
+
+<div className='descryption'>
+<h1>{item.title}</h1>
+<p>{item.price} {valör}</p>
+<input type="text" value={item.stock}></input>
+
+</div>
+<img src={item.image}></img>
+<button>Add to cart</button>
+    </div>
+   
   )
 }
 
-export default Product
+export default Productfunction
