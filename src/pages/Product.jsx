be90c0e0b087d1e74d2e87ae542ad6390133ba06
Leon Aysa <leon.basket@live.com>
2023-04-13 14:49:30 +0200

    commit 2

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React , { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
 
-const Product = () => {
+
+const Productfunction = () => {
+
+    const [item, setitem] = useState([])
+    const params = useParams();
+    
+    const Fetchproduct = async () => {
+      try {
+        const response = await fetch ('https://product-api-production-5f7f.up.railway.app/products/' + params.id)
+        const data = await response.json()
+        setitem(data)
+      
+    
+      } catch (error) {
+        return "an error has occured"
+      }
+    }
+  
+  const valör = "kr"
+  const price = "pris:"
+  const lager = "stock:"
+  const pack = "pc."
+  const besk = "descryption:"
+  const cat = "categories:"
+
+useEffect(() => {
+  Fetchproduct()
+  
+},[])
+
+
+
   return (
-    <div>Product</div>
+<div className='item-container'>
+
+<div className='descryption'>
+<h1>{item.title}</h1>
+<p><b>{price}</b> {item.price} {valör}</p>
+<input type="text" className='input'></input>
+<button className='cart'>Add to Cart</button>
+<p><b>{lager}</b> {item.stock} {pack}</p>
+<p><b>{besk}</b><br/>{item.description}</p>
+<p><b>{cat}</b> <br/>{item.category}</p>
+</div>
+<img src={item.image} className='product-image'></img>
+    </div>
+   
   )
 }
 
-export default Product
+export default Productfunction
