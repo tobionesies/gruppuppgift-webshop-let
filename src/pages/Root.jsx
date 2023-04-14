94999221ef4593135c4e73ee11302f46e8adb9a0
Emil Stjernlöf <emil.stjernlof@gmail.com>
2023-04-14 13:22:56 +0200

    Added color variables and some style components

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -3,12 +3,14 @@ import Footer from '../components/Footer'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
 import Cart from '../components/Cart'
+import { GlobalStyle } from '../components/styled/GlobalStyle'
 
 
 
 const Root = () => {
   return (
     <div>
+      <GlobalStyle/>
         <Header />
         <Cart />
         <section>
