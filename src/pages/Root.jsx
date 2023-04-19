6d263e6bef70d196158b9bc0838030f1fa32e126
Emil Stjernlöf <emil.stjernlof@gmail.com>
2023-04-19 13:13:53 +0200

    init of product add to cart functionality

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -8,6 +8,29 @@ import { GlobalStyle, StyledSection } from '../components/styled/GlobalStyle'
 const Root = () => {
   const [cartToggle, setCartToggle]   = useState(false);
   const [cartContent, setCartContent] = useState([])
+  const [products, setProducts]       = useState()
+  const [quantity, setQuantity]       = useState(1)
+
+
+//   const addToCart = (product) => {
+
+//     const duplicate = cartContent.find(item => item.id === product.id);
+//     if (duplicate) {
+//       const updatedItem = {
+//         ...duplicate, 
+//         quantity: duplicate.quantity + product.quantity, 
+//         price: duplicate.price + product.price
+//       }
+//       console.log("undefined? "+duplicate.quantity)
+//       const theUpdatedCart = cartContent.map(item => item.id === duplicate.id ? updatedItem : item)
+//       setCartContent(theUpdatedCart)
+//     } else {
+//       setCartContent([
+//         ...cartContent,
+//         product
+//       ])
+//     }
+// }
 
   return (
     <div>
@@ -19,7 +42,15 @@ const Root = () => {
           setCartContent={setCartContent}
          />
         <StyledSection>
-            <Outlet context={[cartContent, setCartContent]}/>
+            <Outlet 
+              context={[
+                cartContent, setCartContent,
+                products, setProducts,
+                quantity, setQuantity
+              ]}
+
+              // addToCart={addToCart}
+            />
             {/* <Outlet /> */}
         </StyledSection>
         <Footer />
